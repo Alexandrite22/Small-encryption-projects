ae3afded018c7656dd63741d201d82af6427305a
Alexander <Hewson>
2022-12-02 15:50:30 -0500

    Added fully functionel decrypt message, have not fully tested for bugs or vulnerabilities

--- SmallEncryptionProjects/SmallEncryptionProjects/ASCIIEncrypt.cs
+++ SmallEncryptionProjects/SmallEncryptionProjects/ASCIIEncrypt.cs
@@ -31,15 +31,14 @@ namespace SmallEncryptionProjects
                 inputTemp = inputTemp.Substring(2, inputTemp.Length - 2);
                 tempSectioned.Add(chunk);
             } while (inputTemp.Length > 0);
-            /*for (int i = 0; i < inputTemp.Length; i += 2)       //parses the even numbered length string into chunks for individual encryption
-            {
-                string chunk = inputTemp.Substring(i, 2);
-                tempSectioned.Add(chunk);
-                Console.WriteLine(inputTemp.Substring(i, 2));   
-            }*/
             foreach (string chunk in tempSectioned)             //encrypts each individual chunk
             {
-                string encryptedChunk = (chunk[0] + "" + (chunk[0] + chunk[1]));
+                string encryptedNumber = ""+(chunk[0]+chunk[1]);
+                if (encryptedNumber.Length == 2)
+                {
+                    encryptedNumber = '0' + encryptedNumber;
+                }
+                string encryptedChunk = (chunk[0] + "" + encryptedNumber);
                 tempEncrypted.Add(encryptedChunk);
             }
             if (oddLengthSingleChar != null)                    //if there was an odd number of chars, re adds the char back in post encryption
@@ -52,5 +51,77 @@ namespace SmallEncryptionProjects
             }   
             return encryptedString;                             //returns encrypted string
         }
+        //The encrypted string will always be a length of multiple of four (maybe plus one)
+        //each chunk of four will consist of (a single char which ascii value is between 32-126)
+        //and an int between 64 (written as 064) and (written as 252)
+        //if any of those numbers fall outside of that range the string is invalid and will return (Invalid Encrypted File, check validity)
+        public string decrypt(string input)
+        {
+            string decryptedString = "";
+            if(!(input.Length%4 == 0 || input.Length-1%4 == 0) == true)
+            {
+                return "String cannot be decrypted, please input a string of the correct length";
+            }
+            List<string> chunks = parseToChunks(input);
+            if (checkValidity(chunks) == true)
+            {
+                foreach (string chunk in chunks)
+                {
+                    decryptedString = decryptedString+decryptChunks(chunk);
+                }
+                return decryptedString;   
+            }
+            else
+            {
+                return "String cannot be decrypted";
+            }
+        }
+        private List<string> parseToChunks(string input)
+        {
+            List<string> parsedChunks = new List<string>();
+            string floatingString = "";
+            if (input.Length % 4 != 0)
+            {
+                floatingString = input[input.Length-1]+"";
+                input = input.Substring(0,input.Length-1);
+            }
+            do
+            {
+                parsedChunks.Add(input.Substring(0, 4));
+                input = input.Substring(4, input.Length - 4);
+            } while (input.Length > 0);
+            if(floatingString.Length == 1)
+            {
+                parsedChunks.Add(floatingString);
+            }
+            return parsedChunks;
+        }
+        private bool checkValidity(List<string> input)
+        {
+            foreach(string chunk in input)
+            {
+                if(chunk.Length == 4)
+                {
+                    int ASCIIRange = Int32.Parse(chunk.Substring(1,3));
+                    if (ASCIIRange < 64 || ASCIIRange > 252)
+                    {
+                        return false;
+                    }
+                }    
+            }
+            return true;
+        }
+        private string decryptChunks (string chunk)
+        {
+            if(chunk.Length == 1)
+            {
+                return chunk;
+            }
+            string decrypted = chunk[0] + "";
+            int ASCIICode = Int32.Parse(chunk.Substring(1, 3)) - chunk[0];
+            decrypted = decrypted + (char)ASCIICode;
+            return decrypted;
+        }
+
     }
 }
--- SmallEncryptionProjects/SmallEncryptionProjects/Program.cs
+++ SmallEncryptionProjects/SmallEncryptionProjects/Program.cs
@@ -6,8 +6,11 @@ namespace SmallEncryptionProjects
     {
         static void Main(string[] args)
         {
+            string name = "Maria Rivera1872458~~```()()";
             ASCIIEncrypt test = new ASCIIEncrypt();
-            Console.WriteLine(test.Encrypt("Alexander Hewson!"));
+            Console.WriteLine(
+            "'"+name+"' will encrypt to '" + test.Encrypt(name) + "'.\n" +
+            "'" + test.Encrypt(name) + "' will decrypt back to '" + test.decrypt(test.Encrypt(name)) + "'!");
         }
     }
 }
